f6c0f1267faab02e10747dc29daadfde364edd92
Jannik Buschke <jbu@janniks.work>
2021-10-09 19:24:44 +0200

    init project

---
+++ test/CollectionFixture.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AzFiles.Test
+{
+    [CollectionDefinition("Database collection")]
+    public class CollectionFixture : ICollectionFixture<CustomWebApplicationFactory<Startup>>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
