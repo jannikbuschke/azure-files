41cb63bc400cf528b7c3908efaa510a56619715a
Jannik <sisosys6@gmail.com>
2023-05-05 23:23:21 +0200

    wip

---
+++ app/web/src/app/root-providers.tsx
@@ -0,0 +1,39 @@
+import { MantineProvider } from "@mantine/core"
+import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { NotificationsProvider } from "@mantine/notifications"
+import {
+  GlowProvider,
+  NotificationsProvider as GlowNotificationProvider,
+  VnextAuthenticationProvider,
+} from "glow-core"
+import { TypedNotificationsProvider } from "glow-core/lib/notifications/type-notifications"
+import React from "react"
+
+const client = new QueryClient()
+
+export function RootProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={client}>
+        <VnextAuthenticationProvider>
+          <GlowProvider value={{ componentLibrary: "mantine" }}>
+            <GlowNotificationProvider>
+              <TypedNotificationsProvider>
+                <MantineProvider
+                  theme={{
+                    colorScheme: "light",
+                  }}
+                >
+                  <NotificationsProvider position="bottom-center">
+                    {children}
+                  </NotificationsProvider>
+                </MantineProvider>
+              </TypedNotificationsProvider>
+            </GlowNotificationProvider>
+          </GlowProvider>
+        </VnextAuthenticationProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  )
+}
